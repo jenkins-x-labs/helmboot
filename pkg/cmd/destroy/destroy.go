4a3eee46b5f58e213549027af08e18d12caf8a91
James Strachan <james.strachan@gmail.com>
2020-04-07 07:14:50 +0100

    fix: improve the destroy command
    to handle private git repositories and remove the jx-boot and additional secrets

--- pkg/cmd/destroy/destroy.go
+++ pkg/cmd/destroy/destroy.go
@@ -9,27 +9,28 @@ import (
 
 	"github.com/jenkins-x-labs/helmboot/pkg/common"
 	"github.com/jenkins-x-labs/helmboot/pkg/githelpers"
-	"github.com/jenkins-x-labs/helmboot/pkg/reqhelpers"
+	"github.com/jenkins-x-labs/helmboot/pkg/secretmgr"
+	"github.com/jenkins-x-labs/helmboot/pkg/secretmgr/factory"
 	"github.com/jenkins-x/jx/pkg/cmd/clients"
 	"github.com/jenkins-x/jx/pkg/cmd/helper"
 	"github.com/jenkins-x/jx/pkg/cmd/opts"
 	"github.com/jenkins-x/jx/pkg/cmd/step/create/helmfile"
 	"github.com/jenkins-x/jx/pkg/cmd/templates"
 	"github.com/jenkins-x/jx/pkg/gits"
-	"github.com/jenkins-x/jx/pkg/jxfactory"
 	"github.com/jenkins-x/jx/pkg/log"
 	"github.com/jenkins-x/jx/pkg/util"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Options contains the command line arguments for this command
 type Options struct {
 	CreateHelmfileOptions helmfile.CreateHelmfileOptions
-	JXFactory             jxfactory.Factory
+	KindResolver          factory.KindResolver
 	Gitter                gits.Gitter
 	Dir                   string
-	GitURL                string
 	BatchMode             bool
 }
 
@@ -61,7 +62,7 @@ func NewCmdDestroy() *cobra.Command {
 			helper.CheckErr(err)
 		},
 	}
-	command.Flags().StringVarP(&options.GitURL, "git-url", "u", "", "override the Git clone URL for the JX Boot source to start from, ignoring the versions stream. Normally specified with git-ref as well")
+	command.Flags().StringVarP(&options.KindResolver.GitURL, "git-url", "u", "", "override the Git clone URL for the JX Boot source to start from, ignoring the versions stream. Normally specified with git-ref as well")
 	command.Flags().BoolVarP(&options.BatchMode, "batch-mode", "b", false, "Runs in batch mode without prompting for user input")
 
 	return command
@@ -74,18 +75,12 @@ func (o *Options) Run() error {
 		o.CreateHelmfileOptions.CommonOptions = opts.NewCommonOptionsWithTerm(f, os.Stdin, os.Stdout, os.Stderr)
 		o.CreateHelmfileOptions.CommonOptions.BatchMode = o.BatchMode
 	}
-	if o.JXFactory == nil {
-		o.JXFactory = jxfactory.NewFactory()
-	}
-	gitURL := o.GitURL
+	gitURL := o.KindResolver.GitURL
 	if gitURL == "" {
 		var err error
-		gitURL, err = reqhelpers.FindGitURL(o.JXFactory)
+		gitURL, err = o.KindResolver.LoadBootRunGitURLFromSecret()
 		if err != nil {
 			return errors.Wrap(err, "failed to find Git URL")
-		}
-		if gitURL == "" {
-			return fmt.Errorf("could not find git URL")
 		}
 	}
 
@@ -134,6 +129,16 @@ func (o *Options) Run() error {
 		return err
 	}
 
+	err = o.runCommand(".", env, "helm", "delete", "jx-boot")
+	if err != nil {
+		log.Logger().Debugf("failed to remove the jx-boot chart: %s", err.Error())
+	}
+
+	err = o.removeSecrets(secretmgr.BootGitURLSecret, secretmgr.LocalSecret)
+	if err != nil {
+		return err
+	}
+
 	log.Logger().Infof("chart removal complete. You can run 'jxl boot run' to reinstall")
 	return nil
 }
@@ -163,6 +168,32 @@ func (o *Options) runCommand(dir string, env map[string]string, cmd string, args
 	_, err = c.RunWithoutRetry()
 	if err != nil {
 		return errors.Wrapf(err, "failed to run command: %s %s in dir %s", cmd, strings.Join(args, " "), dir)
+	}
+	return nil
+}
+
+func (o *Options) removeSecrets(names ...string) error {
+	kubeClient, ns, err := o.KindResolver.GetFactory().CreateKubeClient()
+	if err != nil {
+		return errors.Wrap(err, "failed to create kubernetes client")
+	}
+
+	secretsInterface := kubeClient.CoreV1().Secrets(ns)
+	for _, name := range names {
+		_, err := secretsInterface.Get(name, metav1.GetOptions{})
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				continue
+			}
+			return errors.Wrapf(err, "failed to check if Secret %s exists in namespace %s", name, ns)
+		}
+
+		// lets try remove the secret
+		err = secretsInterface.Delete(name, nil)
+		if err != nil {
+			return errors.Wrapf(err, "failed to delete Secret %s in namespace %s", name, ns)
+		}
+		log.Logger().Infof("removed secret %s from namespace %s", name, ns)
 	}
 	return nil
 }
